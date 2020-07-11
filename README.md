2d4498f6b311f6ab1b05be5b61e1643923d8982f
mhakash <mhakash211998@gmail.com>
2020-07-11 12:57:28 +0600

    dropdown

---
+++ README.md
@@ -0,0 +1,9 @@
+# adopt-me
+
+react app for learning from frontendmasters
+
+## Learings
+* pure react only with cdn 
+* Eslint and Preetier
+* Parcel and transpiling
+* hooks: useState, custom hook, use Effets
---
+++ src/SearchParams.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropDown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Dhaka");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStr = breeds.map(({ name }) => name);
+      setBreeds(breedStr);
+    }, console.error);
+  }, [animal, setBreeds, setBreed]);
+
+  return (
+    <div className="search-params">
+      <h2>{location}</h2>
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
